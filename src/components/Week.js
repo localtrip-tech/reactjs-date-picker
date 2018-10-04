1b7e9f776be4414b9e06cca0106d1ac0e9ac1dd7
Shashank Shekhar <shashank7200@gmail.com>
2018-10-04 13:44:59 +0530

    created the component for week list

---
+++ src/components/Week.js
@@ -0,0 +1,42 @@
+/**
+ * @localtrip-tech
+ * Infused by ashwamegh on 03/10/2018
+ */
+
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class Week extends Component {
+  static propTypes = {
+    day: PropTypes.number,
+    days: PropTypes.array.isRequired,
+    highlight: PropTypes.bool,
+    selectDay: PropTypes.func.isRequired
+  }
+
+  handleClick(event) {
+    const { selectDay } = this.props
+    selectDay(event.target.textContent)
+  }
+
+  render() {
+    const { days, day, highlight } = this.props
+    const weekDays = (typeof days === undefined ? [] : days).map((weekDay, index) => {
+      if (weekDay) {
+        if (weekDay === day && highlight) {
+          return <td key={index} className={`datePicker__day--today datePicker__day`} onClick={this.handleClick} >{weekDay}</td>
+        } else {
+          return <td key={index} className='datePicker__day' onClick={this.handleClick}>{weekDay}</td>
+        }
+      } else {
+        return <td key={index} className={`datePicker__day--disabled datePicker__day`} />
+      }
+    }, this)
+
+    return (
+      <tr>{weekDays}</tr>
+    )
+  }
+}
+
+export default Week
